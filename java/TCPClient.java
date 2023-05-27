65ca30a23c93ae9386adc07805ec2ecf860af07d
jaruwit01 <jaruwit178@gmail.com>
2023-05-27 16:46:12 +0700

    u

--- java/TCPClient.java
+++ java/TCPClient.java
@@ -1,17 +1,125 @@
+// import java.io.DataInputStream;
+// import java.io.DataOutputStream;
+// import java.io.IOException;
+// import java.net.Socket;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Scanner;
+// import java.util.logging.Level;
+// import java.util.logging.Logger;
+
+// public class TCPClient {
+//     public static void main(String[] args) {
+//         final String HOST = "192.168.1.106";
+//         final int PORT = 5000;
+//         DataInputStream input;
+//         DataOutputStream output;
+
+//         try {
+//             Scanner scanner = new Scanner(System.in);
+
+//             // Connect to the server
+//             Socket sc = new Socket(HOST, PORT);
+//             input = new DataInputStream(sc.getInputStream());
+//             output = new DataOutputStream(sc.getOutputStream());
+
+//             // Get the number of cities from the user
+//             System.out.print("Enter the number of cities (up to 4): ");
+//             int numCities = scanner.nextInt();
+//             output.writeInt(numCities);
+
+//             scanner.nextLine(); // Consume the newline character
+
+//             // Get the city names and coordinates from the user
+//             for (int i = 1; i <= numCities; i++) {
+//                 System.out.print("Enter the name of City " + i + ": ");
+//                 String cityName = scanner.nextLine();
+//                 output.writeUTF(cityName);
+
+//                 System.out.print("Enter the x-coordinate for " + cityName + ": ");
+//                 double x = scanner.nextDouble();
+//                 output.writeDouble(x);
+
+//                 System.out.print("Enter the y-coordinate for " + cityName + ": ");
+//                 double y = scanner.nextDouble();
+//                 output.writeDouble(y);
+
+//                 scanner.nextLine(); // Consume the newline character
+//             }
+
+//             // Receive and print the optimal route from the server
+//             String optimalRoute = input.readUTF();
+//             System.out.println("\nOptimal Route: " + optimalRoute);
+
+//             sc.close();
+
+//         } catch (IOException ex) {
+//             Logger.getLogger(TCPClient.class.getName()).log(Level.SEVERE, null, ex);
+//         }
+//     }
+// }
+
+
+
+// import java.io.DataInputStream;
+// import java.io.DataOutputStream;
+// import java.io.IOException;
+// import java.net.Socket;
+// import java.util.Scanner;
+// import java.util.logging.Level;
+// import java.util.logging.Logger;
+
+// public class TCPClient {
+//     public static void main(String[] args) {
+//         final String Host = "192.168.1.106";
+//         final int PORT = 5002;
+//         DataInputStream input;
+//         DataOutputStream output;
+
+//         try {
+//             Socket sc = new Socket(Host, PORT);
+//             input = new DataInputStream(sc.getInputStream());
+//             output = new DataOutputStream(sc.getOutputStream());
+
+//             Scanner scanner = new Scanner(System.in);
+
+//             // Client message loop
+//             while (true) {
+//                 // Send client message
+//                 System.out.print("Client: ");
+//                 String clientMessage = scanner.nextLine();
+//                 output.writeUTF(clientMessage);
+
+//                 // Exit loop if client message is "bye"
+//                 if (clientMessage.equalsIgnoreCase("bye")) {
+//                     break;
+//                 }
+
+//                 // Read message from server
+//                 String serverMessage = input.readUTF();
+//                 System.out.println("Server: " + serverMessage);
+//             }
+
+//             sc.close();
+
+//         } catch (IOException ex) {
+//             Logger.getLogger(TCPClient.class.getName()).log(Level.SEVERE, null, ex);
+//         }
+//     }
+// }
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TCPClient {
     public static void main(String[] args) {
-        final String HOST = "localhost";
-        final int PORT = 5000;
+        final String SERVER_IP = "192.168.1.106"; // Replace with the actual IP address or hostname
+        final int SERVER_PORT = 5000;
         DataInputStream input;
         DataOutputStream output;
 
@@ -19,7 +127,7 @@ public class TCPClient {
             Scanner scanner = new Scanner(System.in);
 
             // Connect to the server
-            Socket sc = new Socket(HOST, PORT);
+            Socket sc = new Socket(SERVER_IP, SERVER_PORT);
             input = new DataInputStream(sc.getInputStream());
             output = new DataOutputStream(sc.getOutputStream());
 
@@ -28,28 +136,9 @@ public class TCPClient {
             int numCities = scanner.nextInt();
             output.writeInt(numCities);
 
-            scanner.nextLine(); // Consume the newline character
-
-            // Get the city names and coordinates from the user
-            for (int i = 1; i <= numCities; i++) {
-                System.out.print("Enter the name of City " + i + ": ");
-                String cityName = scanner.nextLine();
-                output.writeUTF(cityName);
-
-                System.out.print("Enter the x-coordinate for " + cityName + ": ");
-                double x = scanner.nextDouble();
-                output.writeDouble(x);
-
-                System.out.print("Enter the y-coordinate for " + cityName + ": ");
-                double y = scanner.nextDouble();
-                output.writeDouble(y);
-
-                scanner.nextLine(); // Consume the newline character
-            }
-
-            // Receive and print the optimal route from the server
-            String optimalRoute = input.readUTF();
-            System.out.println("\nOptimal Route: " + optimalRoute);
+            // Receive and print the sorted order of cities from the server
+            String sortedOrder = input.readUTF();
+            System.out.println("\nSorted Order of Cities: " + sortedOrder);
 
             sc.close();
 
@@ -58,3 +147,4 @@ public class TCPClient {
         }
     }
 }
+
--- java/TCPServer.java
+++ java/TCPServer.java
@@ -5,6 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,123 +21,112 @@ public class TCPServer {
             serverSocket = new ServerSocket(PORT);
             System.out.println("Server started");
 
-            sc = serverSocket.accept();
-            System.out.println("Client connected");
+            while (true) {
+                sc = serverSocket.accept();
+                System.out.println("Client connected");
 
-            input = new DataInputStream(sc.getInputStream());
-            output = new DataOutputStream(sc.getOutputStream());
+                input = new DataInputStream(sc.getInputStream());
+                output = new DataOutputStream(sc.getOutputStream());
 
-            // Read the number of cities from the client
-            int numCities = input.readInt();
+                // Read the number of cities from the client
+                int numCities = input.readInt();
 
-            // Read the city names from the client
-            List<String> cities = new ArrayList<>();
-            for (int i = 0; i < numCities; i++) {
-                String cityName = input.readUTF();
-                if (!cityName.isEmpty()) {
-                    cities.add(cityName);
+                // Generate random cities with coordinates
+                List<String> cities = generateCities(numCities);
+                double[][] coordinates = generateCoordinates(numCities);
+
+                // Calculate the optimal route based on the nearest city
+                List<String> optimalRoute = calculateOptimalRoute(cities, coordinates);
+
+                // Send the sorted order of cities back to the client
+                StringBuilder routeBuilder = new StringBuilder();
+                for (String city : optimalRoute) {
+                    routeBuilder.append(city).append(" -> ");
                 }
+                routeBuilder.setLength(routeBuilder.length() - 4); // Remove the trailing "->"
+                output.writeUTF(routeBuilder.toString());
+
+                sc.close();
+                System.out.println("Client disconnected");
             }
-
-            // Read the city coordinates from the client
-            double[][] coordinates = new double[numCities][2];
-            for (int i = 0; i < numCities; i++) {
-                double x = input.readDouble();
-                double y = input.readDouble();
-                coordinates[i][0] = x;
-                coordinates[i][1] = y;
-            }
-
-            // Generate all possible permutations of the cities
-            List<List<String>> permutations = generatePermutations(cities);
-
-            // Calculate the total distance for each permutation
-            double minDistance = Double.MAX_VALUE;
-            List<String> optimalRoute = new ArrayList<>();
-
-            for (List<String> route : permutations) {
-                double distance = calculateTotalDistance(route, coordinates);
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    optimalRoute = route;
-                }
-            }
-
-            // Send the optimal route back to the client
-            StringBuilder routeBuilder = new StringBuilder();
-            for (String city : optimalRoute) {
-                routeBuilder.append(city).append(" -> ");
-            }
-            routeBuilder.setLength(routeBuilder.length() - 4); // Remove the trailing "->"
-            output.writeUTF(routeBuilder.toString());
-
-            sc.close();
-            System.out.println("Client disconnected");
 
         } catch (IOException ex) {
             Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    // Method to generate all possible permutations of a list of cities
-    private static List<List<String>> generatePermutations(List<String> cities) {
-        List<List<String>> permutations = new ArrayList<>();
-        generatePermutationsHelper(cities.size(), cities, permutations);
-        return permutations;
+    // Method to generate random cities
+    private static List<String> generateCities(int numCities) {
+        List<String> cities = new ArrayList<>();
+        for (int i = 1; i <= numCities; i++) {
+            cities.add("City" + i);
+        }
+        return cities;
     }
 
-    private static void generatePermutationsHelper(int n, List<String> cities, List<List<String>> permutations) {
-        if (n == 1) {
-            permutations.add(new ArrayList<>(cities));
-        } else {
-            for (int i = 0; i < n - 1; i++) {
-                generatePermutationsHelper(n - 1, cities, permutations);
-                if (n % 2 == 0) {
-                    swap(cities, i, n - 1);
-                } else {
-                    swap(cities, 0, n - 1);
+    // Method to generate random coordinates for cities
+    private static double[][] generateCoordinates(int numCities) {
+        double[][] coordinates = new double[numCities][2];
+        Random random = new Random();
+        for (int i = 0; i < numCities; i++) {
+            coordinates[i][0] = random.nextDouble();
+            coordinates[i][1] = random.nextDouble();
+        }
+        return coordinates;
+    }
+
+    // Method to calculate the optimal route based on the nearest city
+    private static List<String> calculateOptimalRoute(List<String> cities, double[][] coordinates) {
+        List<String> route = new ArrayList<>();
+        List<String> remainingCities = new ArrayList<>(cities);
+        Random random = new Random();
+
+        // Select a random starting city
+        int startIndex = random.nextInt(remainingCities.size());
+        String currentCity = remainingCities.get(startIndex);
+        route.add(currentCity);
+        remainingCities.remove(startIndex);
+
+        while (!remainingCities.isEmpty()) {
+            int minIndex = -1;
+            double minDistance = Double.MAX_VALUE;
+
+            // Find the nearest remaining city
+            for (int i = 0; i < remainingCities.size(); i++) {
+                String city = remainingCities.get(i);
+                int currentIndex = getIndex(currentCity, cities);
+                int cityIndex = getIndex(city, cities);
+                double distance = getDistance(coordinates[currentIndex], coordinates[cityIndex]);
+
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    minIndex = i;
                 }
             }
-            generatePermutationsHelper(n - 1, cities, permutations);
+
+            // Add the nearest city to the route
+            currentCity = remainingCities.get(minIndex);
+            route.add(currentCity);
+            remainingCities.remove(minIndex);
         }
+
+        return route;
     }
 
-    private static void swap(List<String> cities, int i, int j) {
-        String temp = cities.get(i);
-        cities.set(i, cities.get(j));
-        cities.set(j, temp);
-    }
-
-    // Method to calculate the total distance of a route
-    private static double calculateTotalDistance(List<String> route, double[][] coordinates) {
-        double totalDistance = 0.0;
-        for (int i = 0; i < route.size() - 1; i++) {
-            String cityA = route.get(i);
-            String cityB = route.get(i + 1);
-            int indexA = getIndex(cityA, route);
-            int indexB = getIndex(cityB, route);
-            double distance = getDistance(coordinates[indexA], coordinates[indexB]);
-            totalDistance += distance;
-        }
-        return totalDistance;
-    }
-
-    // Method to get the index of a city in the route list
-    private static int getIndex(String city, List<String> route) {
-        for (int i = 0; i < route.size(); i++) {
-            if (route.get(i).equals(city)) {
+    // Method to get the index of a city in the list
+    private static int getIndex(String city, List<String> cities) {
+        for (int i = 0; i < cities.size(); i++) {
+            if (cities.get(i).equals(city)) {
                 return i;
             }
         }
         return -1;
     }
 
-    // Method to calculate the Euclidean distance between two cities
-    private static double getDistance(double[] cityA, double[] cityB) {
-        double x1 = cityA[0];
-        double y1 = cityA[1];
-        double x2 = cityB[0];
-        double y2 = cityB[1];
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    // Method to calculate the Euclidean distance between two coordinates
+    private static double getDistance(double[] coord1, double[] coord2) {
+        double dx = coord1[0] - coord2[0];
+        double dy = coord1[1] - coord2[1];
+        return Math.sqrt(dx * dx + dy * dy);
     }
 }
