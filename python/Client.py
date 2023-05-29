054c31030a2459bc1a32b5a17acfba22d899f818
jaruwit01 <jaruwit178@gmail.com>
2023-05-29 20:30:57 +0700

    x

--- python/Client.py
+++ python/Client.py
@@ -1,36 +1,46 @@
 import socket
 
-def send_data(city_name, city_data):
-    server_host = '192.168.1.106'  # IP address of the central server
-    server_port = 5000                 # Port number of the central server
+def send_data_to_all_cities(server_ip, cities, city_name, distance):
+    # Iterate through all cities and send the data
+    for city in cities:
+        if city != city_name:
+            send_data(server_ip, city, city_name, distance)
 
+def send_data(server_ip, city, city_name, distance):
     # Create a socket object
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         # Connect to the server
-        client_socket.connect((server_host, server_port))
+        client_socket.connect((server_ip, 5400))
 
-        # Send the data
-        data = f"{city_name}:{city_data}"
+        # Prepare the data to send
+        data = city_name + ':' + distance
+
+        # Send the data to the server
         client_socket.sendall(data.encode())
 
         # Receive the result from the server
         result = client_socket.recv(1024).decode()
-        print(f"{city_name} received result:\n", result)
+        print("Received result from", city, ":", result)
 
     except ConnectionRefusedError:
-        print("Could not connect to the server.")
+        print("Connection refused by", city)
 
     finally:
         # Close the client socket
         client_socket.close()
 
-# Test the code
-cityA_name = 'City D'
-cityA_data = 'A B C D'  # Example city data
-send_data(cityA_name, cityA_data)
+def main():
+    # Define the IP address of the server
+    server_ip = '192.168.1.181'
 
-# cityB_name = 'City B'
-# cityB_data = 'A D C B'  # Example city data
-# send_data(cityB_name, cityB_data)
+    # Define the list of cities
+    cities = ['CityA', 'CityB', 'CityC', 'CityD']
+
+    # Send the data from each city to all other cities
+    for i, city in enumerate(cities):
+        send_data_to_all_cities(server_ip, cities, city, str(i))
+
+if __name__ == '__main__':
+    main()
--- python/Server.py
+++ python/Server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 def solve_tsptw(data):
     # TODO: Implement the TSPTW solving algorithm based on the received data
@@ -14,7 +15,7 @@ def solve_tsptw(data):
     }
 
     shortest_route = []
-    shortest_distance = float('inf')
+    shortest_distance = [float('inf')]  # Mutable object to store shortest distance
     start_city = cities[0]
     visited = [False] * len(cities)
 
@@ -22,7 +23,7 @@ def solve_tsptw(data):
 
     result = {
         'route': shortest_route,
-        'distance': shortest_distance
+        'distance': shortest_distance[0]
     }
 
     return result
@@ -34,21 +35,40 @@ def find_shortest_route(city, cities, distance_matrix, route, visited, current_d
     if all(visited):
         route.append(cities[0])  # Add the starting city to complete the route
         current_distance += distance_matrix[city][cities[0]]
-        shortest_distance = min(shortest_distance, current_distance)
+        shortest_distance[0] = min(shortest_distance[0], current_distance)
         return
 
     for next_city in cities:
         if not visited[cities.index(next_city)]:
             new_distance = current_distance + distance_matrix[city][next_city]
-            if new_distance < shortest_distance:
+            if new_distance < shortest_distance[0]:
                 find_shortest_route(next_city, cities, distance_matrix, route, visited, new_distance, shortest_distance)
 
     visited[cities.index(city)] = False
     route.pop()
 
+def handle_client(client_socket, client_address):
+    # Receive data from the client
+    data = client_socket.recv(1024).decode()
+    print("Received data:", data)
+
+    # Extract the city name from the received data
+    city_name, city_data = data.split(':')
+    print(city_name, "is", city_data)
+
+    # Solve the TSPTW problem
+    result = solve_tsptw(city_data)
+
+    # Send the result back to the client
+    client_socket.sendall(str(result).encode())
+
+    # Close the client connection
+    client_socket.close()
+    print("Connection closed with", client_address)
+
 def start_server():
     host = '0.0.0.0'   # Listen on all available network interfaces
-    port = 5000        # Port number to listen on
+    port = 5400        # Port number to listen on
 
     # Create a socket object
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -67,23 +87,9 @@ def start_server():
             client_socket, client_address = server_socket.accept()
             print("Connection established with", client_address)
 
-            # Receive data from the client
-            data = client_socket.recv(1024).decode()
-            print("Received data:", data)
-
-            # Extract the city name from the received data
-            city_name, city_data = data.split(':')
-            print(city_name, "is", city_data)
-
-            # Solve the TSPTW problem
-            result = solve_tsptw(city_data)
-
-            # Send the result back to the client
-            client_socket.sendall(str(result).encode())
-
-            # Close the client connection
-            client_socket.close()
-            print("Connection closed with", client_address)
+            # Create a new thread to handle the client
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread.start()
 
     except OSError:
         print("Port is already in use.")
